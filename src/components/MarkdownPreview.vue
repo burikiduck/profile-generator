64dd483253c79df49d4172f04f2745302158d72e
ahmdjn <jhamada66@gmail.com>
2025-02-06 00:06:27 +0900

    added copy function

--- src/components/MarkdownPreview.vue
+++ src/components/MarkdownPreview.vue
@@ -1,26 +1,35 @@
 <script setup lang="ts">
 import { marked } from 'marked'
 import { computed } from 'vue';
+import useClipboard from 'vue-clipboard3'
 
 const props = defineProps<{
   userName?: string,
   skills?: string[],
   frameWorks?: string[]
 }>()
-// const props = defineProps<Profile>()
-// const props = defineProps(['profile'])
 console.log(props)
+const { toClipboard } = useClipboard()
+let contents = ''
 const renderMarkdown = computed(() => {
   // return marked(`# ${props.profile.userName}\n`)
-  let contents = ''
   if (props.userName) {
-    contents += `# Hi, ${props.userName}`
-    contents += `\n# Your skills, ${props.skills}`
-    contents += `\n# You're good at, ${props.frameWorks}`
+    contents += `# Hi, ${props.userName}\n`
+    contents += `# Your skills, ${props.skills}\n`
+    contents += `# You're good at, ${props.frameWorks}\n`
   }
   return contents ? marked(contents) : '<div></div>'
 })
+const copy = async() => {
+  try {
+    await toClipboard(contents)
+    alert('coppy done!')
+  } catch (error) {
+    console.error(error)
+  }
+}
 </script>
 <template>
   <div v-html="renderMarkdown"></div>
+  <button type="button" @click="copy()">Copy</button>
 </template>
